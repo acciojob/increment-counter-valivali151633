a53710e46781a70ca71f15c2d47d5b407ee19b30
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-18 13:57:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,17 @@
 //your JS code here. If required.
-let countervalue = 0;
+let counterValue = 0;
 
 
 function incrementfun(){
 	const counterElement=document.getElementById('counter');
-	counterElement.innerText= countervalue;
-	alert( counterElement.innerText)
-	countervalue++;
-	
-	
+	// counterElement.innerText= countervalue;
+	// alert( counterElement.innerText)
+	// countervalue++;
+alert('Un-incremented value: ' + counterValue);
+      
+      // Increment the counter value
+      counterValue++;
+
+      // Update the counter element's text
+      counterElement.textContent = counterValue;
 }
