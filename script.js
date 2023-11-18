c32c911a4a4533eb2bb7037af625a2afb46e27d3
valivali151633 <144232966+valivali151633@users.noreply.github.com>
2023-11-18 13:41:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+let countervalue = 0;
+
+
+function incrementfun(){
+	const counterElement=document.getElementById('counter');
+	counterElement.innerText= countervalue;
+	alert( counterElement.innerText)
+	countervalue++;
+	
+	
+}
